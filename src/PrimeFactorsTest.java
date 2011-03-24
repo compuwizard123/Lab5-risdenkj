30660f50980f9ba6958a6b032f00cbfe857b5363
Kevin Risden <compuwizard123@gmail.com>
2011-03-24 16:00:05 -0400

    Add JUnit test

--- src/PrimeFactorsTest.java
+++ src/PrimeFactorsTest.java
@@ -1,5 +1,12 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 
 public class PrimeFactorsTest {
+	@Test
+	public void testJUnit() {
+		assertTrue("The compuler isn't feeling well today...", true);
+	}
 
 }
