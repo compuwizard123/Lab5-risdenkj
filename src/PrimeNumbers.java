92fd76b51cba28f1ee95d12966a2713f3635713b
Thomas W. Most <twm@freecog.net>
2011-03-27 15:20:34 -0400

    Format source

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,18 +1,17 @@
 import java.util.ArrayList;
 
-
 public class PrimeNumbers {
 
 	public static ArrayList<Integer> generatePrimes(int n) {
-		
+
 		ArrayList<Integer> ret = new ArrayList<Integer>();
-		for(int candidate = 2; n > 1; candidate++) {
+		for (int candidate = 2; n > 1; candidate++) {
 			while ((n % candidate) == 0) {
 				ret.add(candidate);
 				n /= candidate;
 			}
 		}
-		
+
 		if (n > 1) {
 			ret.add(n);
 		}
@@ -20,11 +19,12 @@ public class PrimeNumbers {
 	}
 
 	/**
-	 * Unlike the above method, this one actually generates primes instead 
-	 * of performing a prime factorization.  It will produce a list of
-	 * primes less than n.
+	 * Unlike the above method, this one actually generates primes instead of
+	 * performing a prime factorization. It will produce a list of primes less
+	 * than n.
 	 * 
-	 * @param n The ceiling.
+	 * @param n
+	 *            The ceiling.
 	 * @return A list of primes.
 	 */
 	public static ArrayList<Integer> actuallyGeneratePrimes(int n) {
@@ -32,15 +32,15 @@ public class PrimeNumbers {
 		if (n > 2) {
 			ret.add(2);
 		}
-		for(int i = 3; i < n; i+=2) {
+		for (int i = 3; i < n; i += 2) {
 			boolean prime = true;
-			for(int j = 3; j < i; j+=2) {
-				if((i%j) == 0) {
+			for (int j = 3; j < i; j += 2) {
+				if ((i % j) == 0) {
 					prime = false;
 					break;
 				}
 			}
-			if(prime) {
+			if (prime) {
 				ret.add(i);
 			}
 		}
